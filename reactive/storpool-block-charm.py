51125c20e7050d1655e89364495fabdccd1c9f02
Peter Pentchev <pp@storpool.com>
2017-07-29 20:42:27 -0400

    Use the storpool-config layer.

--- reactive/storpool-block-charm.py
+++ reactive/storpool-block-charm.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import subprocess
 import time
 
 from charms import reactive
@@ -11,12 +12,17 @@ def rdebug(s):
 	with open('/tmp/storpool-charms.log', 'a') as f:
 		print('{tm} [block-charm] {s}'.format(tm=time.ctime(), s=s), file=f)
 
-@reactive.when('storpool-repo-add.available')
+@reactive.when('storpool-config.config-written')
 def whee():
 	rdebug('wheeeeeee')
-	hookenv.status_set('maintenance', 'the charm finalizing the final stuff for the final time')
 
+	hookenv.status_set('maintenance', 'checking our storpool-repo-add installation')
 	policy = sprepo.apt_pkg_policy(['txn-install', 'storpool-config', 'meowmeow'])
 	rdebug('got some kind of policy: {p}'.format(p=policy))
+
+	hookenv.status_set('maintenance', 'checking our storpool-config installation')
+	lines_b = subprocess.check_output(['/usr/sbin/storpool_confshow', '-n', 'SP_OURID'])
+	lines = lines_b.decode().split('\n')
+	rdebug('got some kind of output from storpool_confshow -n SP_OURID: {out}'.format(out=lines))
 
 	hookenv.status_set('active', 'so far so good so what')
