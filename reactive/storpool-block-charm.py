ab6474513cc27952e52b5282d128e2e869089066
Peter Pentchev <pp@storpool.com>
2017-08-06 09:16:40 -0400

    Test the CLI and the Python bindings.

--- reactive/storpool-block-charm.py
+++ reactive/storpool-block-charm.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import json
 import subprocess
 import time
 
@@ -8,6 +9,7 @@ from charmhelpers.core import hookenv
 
 from spcharms import repo as sprepo
 from spcharms import config as spconfig
+from spcharms import txn
 
 def rdebug(s):
 	with open('/tmp/storpool-charms.log', 'a') as f:
@@ -48,5 +50,23 @@ def whee():
 			hookenv.status_set('error', 'interface {iface} does not seem to have an MTU of 9000: {line}'.format(iface=iface, line=line))
 			return
 	rdebug('looks like the network interfaces check out')
+
+	hookenv.status_set('maintenance', 'trying to figure out which StorPool services are running')
+	out_b = subprocess.check_output(['storpool', '-Bj', 'service', 'list'])
+	out = json.loads(out_b.decode())
+	rdebug('storpool service list returned {out}'.format(out=out))
+
+	hookenv.status_set('maintenance', 'trying to obtain the StorPool disk IDs through Python')
+	cfg = spconfig.get_dict()
+	out_b = subprocess.check_output(['python2', '-c', 'from storpool import spapi; a = spapi.Api(host="{host}", port={port}, auth="{auth}"); print str.join(" ", map(str, sorted(a.disksList().keys())))'.format(host=cfg['SP_API_HTTP_HOST'], port=cfg['SP_API_HTTP_PORT'], auth=cfg['SP_AUTH_TOKEN'])])
+	rdebug('disks found: {disks}'.format(disks=out_b.decode().split('\n')[0]))
+
+	hookenv.status_set('maintenance', 'trying to build a list of LXC objects')
+	lst = list(txn.LXD.list_all())
+	rdebug('LXD.list_all() returned {lst}'.format(lst=lst))
+	objs = list(txn.LXD.construct_all())
+	rdebug('LXD.construct_all() returned a list of {ln} objects:'.format(ln=len(objs)))
+	for obj in objs:
+		rdebug('- name "{name}" prefix "{prefix}"'.format(name=obj.name, prefix=obj.txn.prefix))
 
 	hookenv.status_set('active', 'so far so good so what')
