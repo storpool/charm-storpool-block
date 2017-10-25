13b772fd0fc8b6f4c2856d2f7d8a8021cbd7187b
Peter Pentchev <pp@storpool.com>
2017-10-25 03:45:24 -0400

    Code style: docstrings.

--- reactive/storpool-block-charm.py
+++ reactive/storpool-block-charm.py
@@ -1,3 +1,20 @@
+"""
+A Juju charm that installs the StorPool block (client initiator) service and
+provides the configuration needed for other StorPool charms.
+
+Subordinate hooks to the master:
+- juju-info: attach to another charm (e.g. `nova-compute` or `cinder-storpool`)
+  as a subordinate
+
+Configuration hooks:
+- storpool-presence: announce to other charms when the StorPool client service
+  has been installed and configured on the Juju nodes
+
+Internal hooks:
+- block-p: announce to the other `storpool-block` units that this unit has
+  been installed and configured
+"""
+
 from __future__ import print_function
 
 import json
@@ -12,10 +29,17 @@ from spcharms import utils as sputils
 
 
 def rdebug(s):
+    """
+    Pass the diagnostic message string `s` to the central diagnostic logger.
+    """
     sputils.rdebug(s, prefix='block-charm')
 
 
 def hook_debug(hc):
+    """
+    Log diagnostic information about the `hc` hook.
+    FIXME: remove this function as part of the `storpool-config` hook cleanup.
+    """
     rdebug('hook information:')
     try:
         rdebug('- itself: {hc}'.format(hc=hc))
@@ -34,18 +58,31 @@ def hook_debug(hc):
 
 @reactive.hook('leader-elected')
 def we_are_the_leader():
+    """
+    Make note of the fact that this unit has been elected as the leader for
+    the `storpool-block` charm.  This will prompt the unit to send presence
+    information to the other charms along the `storpool-presence` hook.
+    """
     rdebug('looks like we have been elected leader')
     reactive.set_state('storpool-block-charm.leader')
 
 
 @reactive.hook('leader-settings-changed')
 def we_are_not_the_leader():
+    """
+    Make note of the fact that this unit is no longer the leader for
+    the `storpool-block` charm, so no longer attempt to send presence data.
+    """
     rdebug('welp, we are not the leader')
     reactive.remove_state('storpool-block-charm.leader')
 
 
 @reactive.hook('leader-deposed')
 def we_are_no_longer_the_leader():
+    """
+    Make note of the fact that this unit is no longer the leader for
+    the `storpool-block` charm, so no longer attempt to send presence data.
+    """
     rdebug('welp, we have been deposed as leader')
     reactive.remove_state('storpool-block-charm.leader')
 
@@ -53,6 +90,9 @@ def we_are_no_longer_the_leader():
 @reactive.when('storpool-service.change')
 @reactive.when('storpool-block-charm.leader')
 def peers_change():
+    """
+    Handle a presence data change reported along the internal `block-p` hook.
+    """
     rdebug('whee, got a storpool-service.change notification')
     reactive.remove_state('storpool-service.change')
 
@@ -80,6 +120,10 @@ def peers_change():
 @reactive.when('storpool-presence.notify')
 @reactive.when('storpool-block-charm.leader')
 def announce_peers(hk):
+    """
+    If this unit is the leader, send the collected presence data to other
+    charms along the `storpool-presence` hook.
+    """
     rdebug('about to announce our presence to the StorPool Cinder thing')
     rel_ids = hookenv.relation_ids('storpool-presence')
     rdebug('- got rel_ids {rel_ids}'.format(rel_ids=rel_ids))
@@ -96,6 +140,9 @@ def announce_peers(hk):
 @reactive.when('storpool-config.available')
 @reactive.when_not('storpool-block-charm.stopped')
 def announce_no_config(hconfig):
+    """
+    FIXME: remove this function; the `storpool-config` hook was removed.
+    """
     try:
         rdebug('letting the other side know that we have no config yet')
         hook_debug(hconfig)
@@ -108,6 +155,9 @@ def announce_no_config(hconfig):
 @reactive.when('storpool-config.available')
 @reactive.when_not('storpool-block-charm.stopped')
 def announce_config(hconfig):
+    """
+    FIXME: remove this function; the `storpool-config` hook was removed.
+    """
     try:
         rdebug('letting the other side know that we have some configuration')
         hook_debug(hconfig)
@@ -122,12 +172,25 @@ def announce_config(hconfig):
 @reactive.when('storpool-osi.installed-into-lxds')
 @reactive.when_not('storpool-block-charm.stopped')
 def ready():
+    """
+    When the StorPool block service has been installed and the OpenStack
+    integration has been installed everywhere, set the unit's status to
+    `active`.
+    """
     rdebug('ready to go')
     hookenv.status_set('active', 'so far so good so what')
 
 
 @reactive.hook('stop')
 def stop_and_propagate():
+    """
+    Propagate a `stop` action to the lower layers; in particular, let
+    the `storpool-openstack-integration` layer know that it does not need to
+    propagate the `stop` action by itself.
+
+    Also set the "storpool-block-charm.stopped" state so that no further
+    presence or status updates are sent to other units or charms.
+    """
     rdebug('a stop event was received')
 
     rdebug('letting storpool-openstack-integration know')
